277650083fd6475c8e1a84acff7b67a3a8bc909e
Steve Kaliski <sjkaliski@gmail.com>
2014-03-11 13:24:06 -0400

    fixes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.logger('dev'))
 app.use(express.bodyParser())
 app.use(express.methodOverride())
 app.use(app.router)
-app.use(require('less-middleware')({ src: __dirname + '/public' }))
+app.use(require('less-middleware')(__dirname + '/public'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // development only
@@ -60,4 +60,4 @@ app.get('/healthz', function (req, res) {
   res.send('ok')
 })
 
-app.listen(3000)
+app.listen(80)
